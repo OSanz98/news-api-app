7ac5efd050f293fb0c446bf406e885e72887f91f
OSanz98 <oscar.sanz_98@hotmail.com>
2023-09-29 16:36:42 +0100

    Send email with article news

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import requests
+from send_email import send_email
 
 api_key = os.getenv("NEWS_API_APP_KEY")
-url = ("https://newsapi.org/v2/everything?q=apple&from=2023-09-28&to=2023-09-28&sortBy=popularity&apiKey"
-       "=4ac12bae2df943f98563f7e6f6221215")
+url = "https://newsapi.org/v2/everything?q=apple&from=2023-09-28&to=2023-09-28&sortBy=popularity&apiKey" \
+       f"={api_key}"
 
 # Make a request
 request = requests.get(url)
@@ -14,6 +15,10 @@ content = request.json()
 print(content)
 
 # Access the article titles and description
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["author"])
+    if article["title"] is not None and article["author"] is not None and article["url"] is not None:
+        body = body + article["title"] + "\n" + article["author"] + "\n" + article["url"] + 2*"\n"
+
+# body = body.encode("utf-8")
+send_email(content=body)
