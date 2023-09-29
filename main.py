6f3bbda6b3942945c5ba18400fab20cc7d5544c1
OSanz98 <oscar.sanz_98@hotmail.com>
2023-09-29 16:23:43 +0100

    Added readme and gitignore

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import os
+import requests
 
 api_key = os.getenv("NEWS_API_APP_KEY")
-print(api_key)
-# url = ("https://newsapi.org/v2/everything?q=apple&from=2023-09-28&to=2023-09-28&sortBy=popularity&apiKey"
-#        f"={api_key}")
-#
-# # Make a request
-# request = requests.get(url)
-#
-# # Get a dictionary with data
-# content = request.json()
+url = ("https://newsapi.org/v2/everything?q=apple&from=2023-09-28&to=2023-09-28&sortBy=popularity&apiKey"
+       "=4ac12bae2df943f98563f7e6f6221215")
+
+# Make a request
+request = requests.get(url)
+
+# Get a dictionary with data
+content = request.json()
+
+print(content)
 
 # Access the article titles and description
-# for article in content["articles"]:
-#     print(article["title"])
+for article in content["articles"]:
+    print(article["title"])
+    print(article["author"])
