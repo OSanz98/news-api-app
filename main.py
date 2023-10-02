944a315ff103640d9fc21cdec31c99a1025bd023
OSanz98 <oscar.sanz_98@hotmail.com>
2023-10-02 16:18:32 +0100

    Added links, other query paramters, added subject

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import os
 import requests
 from send_email import send_email
 
+topic = "apple"
 api_key = os.getenv("NEWS_API_APP_KEY")
-url = "https://newsapi.org/v2/everything?q=apple&from=2023-09-28&to=2023-09-28&sortBy=popularity&apiKey" \
-       f"={api_key}"
+url = "https://newsapi.org/v2/everything?"\
+      f"q={topic}&" \
+      "from=2023-09-28&"\
+      "to=2023-09-28&"\
+      "sortBy=popularity&"\
+      f"apiKey={api_key}&"\
+      "language=en"
 
 # Make a request
 request = requests.get(url)
@@ -16,9 +22,12 @@ print(content)
 
 # Access the article titles and description
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None and article["author"] is not None and article["url"] is not None:
-        body = body + article["title"] + "\n" + article["author"] + "\n" + article["url"] + 2*"\n"
+        body = "Subject: Today's news" \
+            + "\n" + body + article["title"] + "\n" \
+            + article["author"] + "\n" \
+            + article["url"] + 2*"\n"
 
 # body = body.encode("utf-8")
 send_email(content=body)
